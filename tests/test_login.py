7ead9036b75be74fe673c5932eeef8b7ef083aaa
Deepti Chouhan <deepti.chouhan@encoresky.com>
2025-12-03 14:44:10 +0530

    Manage the nagative and possitive test cases in single file

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,8 @@
-import json
+import re
+import json 
+import pytest
+from playwright.sync_api import Page, expect
+from tests.conftest import BASE_URL
 from pages.login_page import LoginPage
 
 def load_test_data():
@@ -6,9 +10,20 @@ def load_test_data():
         data= json.load(f)
     invalid_creds=data["invalid_logins"]
     return data, invalid_creds
-
 data, invalid_creds = load_test_data()
 
+@pytest.mark.smoke
+def test_valid_login(page: Page):
+    page.goto(BASE_URL)
+    email_input = page.get_by_placeholder("Email")
+    password_input = page.get_by_placeholder("Password")
+    login_button = page.get_by_role("button", name="Login")
+    email_input.fill(data["superAdmin"]["email"])
+    password_input.fill(data["superAdmin"]["password"])
+    login_button.click()
+    expect(page).to_have_url(re.compile("dashboard", re.IGNORECASE))
+
+@pytest.mark.smoke
 def test_blank_email(page):
     login = LoginPage(page)
     login.goto()
@@ -17,6 +32,7 @@ def test_blank_email(page):
     login.click_login()
     assert login.get_error_message() == "Email is required"
 
+@pytest.mark.smoke
 def test_invalid_email_format(page):
     login = LoginPage(page)
     login.goto()
@@ -25,6 +41,7 @@ def test_invalid_email_format(page):
     login.click_login()
     assert login.get_error_message() == "Invalid email"
 
+@pytest.mark.smoke
 def test_unregistered_email(page):
     login = LoginPage(page)
     login.goto()
@@ -33,6 +50,7 @@ def test_unregistered_email(page):
     login.click_login()
     assert login.get_error_message() == "User not found.."
 
+@pytest.mark.smoke
 def test_wrong_password(page):
     login = LoginPage(page)
     login.goto()
@@ -40,3 +58,4 @@ def test_wrong_password(page):
     login.enter_password(invalid_creds["wrong_password"])
     login.click_login()
     assert login.get_error_message() == "The password provided does not meet requirements.."
+
